893eee34d345b19a6047e8d9e3d633d3436aefcb
姜威 <jw210294@gongdao.com>
2020-07-10 20:21:55 +0800

    update main test script

--- ydlj/model/main.py
+++ ydlj/model/main.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import os
-import json
 
 from reading_comprehension.reading_comprehension_net import ReadingComprehensionModel
 from reading_comprehension.data_process import read_examples, convert_examples_to_features
@@ -13,13 +12,10 @@ def main():
     RCModel = ReadingComprehensionModel(model_path=os.path.abspath('./checkpoints/rc_20200709-163056'),
                                         model_selection='B')
 
-    # with open(infile, "r", encoding='utf-8') as f_in:
-    #     data = json.load(f_in)
-
     examples = read_examples(full_file=os.path.abspath(infile), is_labeled=False)
     features = convert_examples_to_features(examples, RCModel.bert_tokenizer, max_seq_length=512, max_query_length=50)
 
-    RCModel.predict(examples=examples, features=features, batch_size=2, test_per_sample=False,
+    RCModel.predict(examples=examples, features=features, batch_size=32, test_per_sample=False,
                     support_fact_threshold=0.2, restrict_answer_span=True, result_file=os.path.abspath(outfile))
     print('Done.')
 
